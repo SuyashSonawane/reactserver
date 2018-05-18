dee928103b56d095efc1d07d24f19b3a6c4588f3
Suyash <hackerzden01@gmail.com>
2018-05-18 19:41:51 +0530

    n

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const pg = knex({
         client: 'pg',
         connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'psqlpass', 
-        database: 'viewsdb'
+        connectionString :process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
